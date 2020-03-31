9b1893e3fd2fe58f042122f5ad0cc97a1febf44d
guiyh <guiyh@qq.com>
2020-03-31 11:29:23 +0800

    Signed-off-by: guiyh <guiyh@qq.com>

--- 912. 排序数组.java
+++ 912. 排序数组.java
@@ -1,9 +1,9 @@
 class Solution {
-    public List<Integer> sortArray(int[] nums) {
-        quickSort(nums,0,nums.length-1);
-        return Arrays.stream(nums).boxed().collect(Collectors.toList());
+    public int[] sortArray(int[] nums) {
+		//quickSort(nums,0,nums.length-1);
+        return mergeSort(nums);
     }
-    public void quickSort(int[] nums, int left, int right){
+     public void quickSort(int[] nums, int left, int right){
         if(left>=right){
             return;
         }
@@ -37,5 +37,34 @@ class Solution {
         quickSort(nums,l,p-1);
         quickSort(nums,p+1,r);
         
+    }
+    public int[] mergeSort(int[] nums){
+        if(nums.length==1){
+            return nums;
+        }
+        int[] left=Arrays.copyOfRange(nums,0,nums.length/2);
+        int[] right=Arrays.copyOfRange(nums,nums.length/2,nums.length);
+        mergeSort(left);
+        mergeSort(right);
+        for(int i=0,j=0;i<left.length||j<right.length;){
+            if(i<left.length&&j<right.length){
+                if(left[i]<right[j]){
+                    nums[i+j]=left[i];
+                    i++;
+                }else{
+                    nums[i+j]=right[j];
+                    j++;
+                }
+            }
+            if(i>=left.length&&j<right.length){
+                nums[i+j]=right[j];
+                j++;
+            }
+            if(j>=right.length&&i<left.length){
+                nums[i+j]=left[i];
+                i++;
+            }
+        }
+        return nums;
     }
 }
